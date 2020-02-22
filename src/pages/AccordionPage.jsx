27718b7b2e5f35cc47c7d16ff61b6094832ec3fa
Josh Bryson <josh@bryson.me.uk>
2020-02-22 17:57:26 +0000

    Basic re-usable accordion component created

--- src/pages/AccordionPage.jsx
+++ src/pages/AccordionPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Accordion from '../components/Accordion/Accordion';
+import AccordionData from '../components/Accordion/AccordionData.json';
 
 function AccordionPage() {
   return (
     <div className="App">
         <h2>Accordion Page</h2>
+        <Accordion data={AccordionData} />
     </div>
   );
 }
