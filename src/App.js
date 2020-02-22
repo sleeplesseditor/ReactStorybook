dfd3c19078312c280380ddcf967fb7a9977353d6
Josh Bryson <josh@bryson.me.uk>
2020-02-22 17:23:59 +0000

    Initial navigation component and lazy loading setup established

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import LazyLoader from './components/LazyLoader/LazyLoader';
 import './App.css';
+
+const AccordionPage = React.lazy(() => import('./pages/AccordionPage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          To open the Storybook, enter <code>npm run storybook</code> in the project terminal.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={LazyLoader(AccordionPage)} />
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AccordionPage() {
+  return (
+    <div className="App">
+        <h2>Accordion Page</h2>
+    </div>
+  );
+}
+
+export default AccordionPage;
